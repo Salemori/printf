ba722989486436e603ce8eb80e4f4feb57f1bc5c
Ayondrun <ayobamijoelobadare@gmail.com>
2022-08-07 13:59:18 -0700

    printf_srev.c

--- _printf.c
+++ _printf.c
@@ -1,45 +1,80 @@
 #include "main.h"
 /**
- * _printf - is a function that selects the correct function to print.
- * @format: identifier to look for.
- * Return: the length of the string.
+ * _printf - is a function that prints
+ * @format: gives the format to the function
+ * cases - d, i, s, c, %
+ * Return: a string.
  */
-int _printf(const char * const format, ...)
+int _printf(const char *format, ...)
 {
-	convert_match m[] = {
-		{"%s", printf_string}, {"%c", printf_char},
-		{"%%", printf_37},
-		{"%i", printf_int}, {"%d", printf_dec}, {"%r", printf_srev},
-		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
-		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
-		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
+	va_list ap;
+	int count = -1;
+
+	pr_f ops[] = {
+	{"c", print_c},
+	{"s", print_s},
+	{"d", print_d},
+	{"%", print_mod},
+	{"i", print_d},
+	{"r", print_r},
+	{NULL, NULL}
 	};
 
-	va_list args;
-	int i = 0, j, len = 0;
-
-	va_start(args, format);
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-Here:
-	while (format[i] != '\0')
+	if (format != NULL)
 	{
-		j = 13;
-		while (j >= 0)
-		{
-			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
-			{
-				len += m[j].f(args);
-				i = i + 2;
-				goto Here;
-			}
-			j--;
-		}
-		_putchar(format[i]);
-		len++;
-		i++;
+		va_start(ap, format);
+		count = _funcion(format, ops, ap);
+		va_end(ap);
 	}
-	va_end(args);
-	return (len);
+	return (count);
+}
+
+/**
+ * _funcion - Helper function to print and call functions.
+ * @format: String recieved.
+ * @ops: special options.
+ * @ap: arguments
+ * Return: number of chars printed
+ */
+
+int _funcion(const char *format, pr_f ops[], va_list ap)
+{
+	int count = 0, i, j;
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{
+				return (-1);
+			}
+			for (j = 0; ops[j].op != NULL; j++)
+			{
+				if (format[i + 1] == ops[j].op[0])
+				{
+					count = count + ops[j].f(ap);
+					break;
+				}
+			}
+			if (ops[j].op == NULL && format[i + 1] != ' ')
+			{
+				if (format[i + 1] != '\0')
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					count = count + 2;
+				}
+				else
+					return (-1);
+			}
+			i = i + 1;
+		}
+		else
+		{
+			_putchar(format[i]);
+			count = count + 1;
+		}
+	}
+	return (count);
 }
